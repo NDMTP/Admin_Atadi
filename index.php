966ca346c95fd2c011b2ca79ea3f238535cf96c1
Trần Ngọc Duy <duybii922002@gmail.com>
2023-09-28 09:34:13 +0700

    update mot so sp

--- index.php
+++ index.php
@@ -124,7 +124,7 @@
                       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                         <div class="card-content">
                           <h5 class="font-15">Doanh Thu</h5>
-                          <h2 class="mb-3 font-18">60 Củ</h2>
+                          <h2 class="mb-3 font-18">60 Tỏi</h2>
                           <p class="mb-0"><span class="col-green">42%</span> Tăng</p>
                         </div>
                       </div>
